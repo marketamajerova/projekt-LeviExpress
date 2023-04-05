23de3010843fb55737e3b30b9794f7a6730a6a25
Martin Podloucký <podlouckymartin@gmail.com>
2023-04-05 20:49:00 +0200

    Povýšení na nejnovější verzi creact-kodim-app

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
 import './style.css';
 
-export const API_BASE_URL = 'http://leviexpress-backend.herokuapp.com/api';
+export const API_BASE_URL = 'https://apps.kodim.cz/daweb/leviexpress/api';
 
 createRoot(document.querySelector('#app')).render(<App />, );
