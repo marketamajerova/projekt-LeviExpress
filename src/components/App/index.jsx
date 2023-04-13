075377d06132b35be5a53bf8fae2a37985a84922
marketamajerova <marketamajerova.6@gmail.com>
2023-04-13 18:51:54 +0200

    Defining Router with paths to subpages

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
 
-export const App = () => (
-  <>
-    <Header />
-    <Home />
-    <Footer />
-  </>
-);
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <RouterProvider router={router} />
+      {/* <Home /> */}
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/reservation',
+    element: <Reservation /> 
+  }
+])
---
+++ src/components/Reservation/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Reservation = () => (
+  <h2>Ahoj ja jsem rezervace</h2>
+);
